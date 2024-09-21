7e537aba515d9d017f81ecb0af2b314039ba8951
KainaMF <kaina.marques@calindra.com.br>
2024-09-21 20:12:32 -0300

    feat: cria useQuestionario.ts

---
+++ app-mobile/data/hooks/useQuestionario.ts
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import todasAsPerguntas from "@/data/constants/perguntas";
+
+export default function useQuestionario() {
+  const [indicePergunta, setIndicePergunta] = useState(0);
+  const [respostas, setRespostas] = useState<number[]>([]);
+  const [perguntas, setPerguntas] = useState(todasAsPerguntas.slice(0, 10));
+
+  return {
+    get pergunta() {
+      return perguntas[indicePergunta];
+    },
+    get pontuacao() {
+      return perguntas
+        .map((p) => p.resposta)
+        .map((r, i) => r === respostas[i])
+        .filter(Boolean).length;
+    },
+    get totalDePerguntas() {
+      return perguntas.length;
+    },
+    get concluido() {
+      return indicePergunta === perguntas.length;
+    },
+    responder(resposta: number) {
+      setRespostas([...respostas, resposta]);
+      setIndicePergunta(indicePergunta + 1);
+    },
+    reiniciar() {
+      setIndicePergunta(0);
+      setRespostas([]);
+    },
+  };
+}
