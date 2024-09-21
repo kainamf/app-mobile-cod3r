35891ec07781010897d47c8bc4e318a817080c96
KainaMF <kaina.marques@calindra.com.br>
2024-09-21 19:59:05 -0300

    refactor: estiliza opcao e pergunta

--- app-mobile/components/questionario/Pergunta.tsx
+++ app-mobile/components/questionario/Pergunta.tsx
@@ -1,25 +1,25 @@
-import PerguntaModel from '@/data/model/Pergunta'
-import { Text, View } from 'react-native'
-import Enunciado from './Enunciado'
-import Opcao from './Opcao'
+import PerguntaModel from "@/data/model/Pergunta";
+import { Text, View } from "react-native";
+import Enunciado from "./Enunciado";
+import Opcao from "./Opcao";
 
 export interface PerguntaProps {
-    pergunta: PerguntaModel
+  pergunta: PerguntaModel;
 }
 export default function Pergunta(props: PerguntaProps) {
-    return(
-        <View>
-            <Enunciado enunciado={props.pergunta.enunciado} />
-            <View>
-                {props.pergunta.opcoes.map((opcao, indice) => (
-                    <Opcao
-                        key={indice}
-                        indice={indice}
-                        texto={opcao}
-                        onPress={() => {}}
-                    />
-                ))}
-            </View>
-        </View>
-    )
+  return (
+    <View style={{ gap: 25 }}>
+      <Enunciado enunciado={props.pergunta.enunciado} />
+      <View style={{ gap: 13 }}>
+        {props.pergunta.opcoes.map((opcao, indice) => (
+          <Opcao
+            key={indice}
+            indice={indice}
+            texto={opcao}
+            onPress={() => {}}
+          />
+        ))}
+      </View>
+    </View>
+  );
 }
