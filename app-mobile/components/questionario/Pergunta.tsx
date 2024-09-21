c97036e18b34fb316d62b4943440b1f3cc666215
KainaMF <kaina.marques@calindra.com.br>
2024-09-21 19:46:26 -0300

    refactor: Melhora Enunciado.tsx e Pergunta.tsx

--- app-mobile/components/questionario/Pergunta.tsx
+++ app-mobile/components/questionario/Pergunta.tsx
@@ -1,6 +1,7 @@
 import PerguntaModel from '@/data/model/Pergunta'
 import { Text, View } from 'react-native'
 import Enunciado from './Enunciado'
+import Opcao from './Opcao'
 
 export interface PerguntaProps {
     pergunta: PerguntaModel
@@ -9,6 +10,16 @@ export default function Pergunta(props: PerguntaProps) {
     return(
         <View>
             <Enunciado enunciado={props.pergunta.enunciado} />
+            <View>
+                {props.pergunta.opcoes.map((opcao, indice) => (
+                    <Opcao
+                        key={indice}
+                        indice={indice}
+                        texto={opcao}
+                        onPress={() => {}}
+                    />
+                ))}
+            </View>
         </View>
     )
 }
