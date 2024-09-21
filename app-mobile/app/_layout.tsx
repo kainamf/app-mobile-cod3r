eb15f44948b0ca2d56406555b5394e4bc4cc63a3
KainaMF <kaina.marques@calindra.com.br>
2024-09-21 18:53:40 -0300

    feat: instala e configura fontes

--- app-mobile/app/_layout.tsx
+++ app-mobile/app/_layout.tsx
@@ -1,6 +1,26 @@
 import { Stack } from "expo-router";
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts ({
+    'SOLARSPACEDEMO-Regular': require('../assets/fonts/SOLARSPACEDEMO-Regular.otf'),
+    'SPACEMISSION': require('../assets/fonts/SPACEMISSION.otf')
+  });
+
+  useEffect(()=> {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error])
+  
+  if (!loaded && !error) {
+    return null
+  }
+
   return (
     <Stack>
       <Stack.Screen name="index" />
--- app-mobile/app/index.tsx
+++ app-mobile/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{ fontFamily: 'SPACEMISSION'}}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
